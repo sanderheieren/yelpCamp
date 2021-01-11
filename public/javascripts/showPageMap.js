ea3f41954ebe69e2549db4a64de420bdceb8e19a
sanderheieren <sanheieren@gmail.com>
2021-01-11 21:43:22 +0100

    map working

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,17 @@
     mapboxgl.accessToken =  mapToken // from ejs
         const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        style: 'mapbox://styles/mapbox/satellite-v9', // stylesheet location
         center: campground.geometry.coordinates, // starting position [lng, lat]
         zoom: 10 // starting zoom
       });
 
 new mapboxgl.Marker()
       .setLngLat(campground.geometry.coordinates)
+      .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+          `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+      )
       .addTo(map)
