9b8ad179b5d0c0224358a96eabf5b9153ade8dc4
sanderheieren <sanheieren@gmail.com>
2021-01-14 20:36:57 +0100

    changed port

--- app.js
+++ app.js
@@ -150,6 +150,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err })
 })
 const port = process.env.PORT || 3000
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('serving on port' + port);
 })
